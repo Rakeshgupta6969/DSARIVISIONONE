d7386a03ab64d644fdd2513bc08d9c0a2ef4a08a
Rakesh Gupta <rg688407@gmail.com>
2026-02-06 22:59:55 +0530

    new question

---
+++ Leetcode_3634/sol.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        if(n == 1) return 0;
+
+
+        sort(nums.begin(),nums.end());
+
+        int ans = n;
+
+        int j = 0;
+        for(int i = 0; i<n; i++){
+            while(j<n && nums[j]<= (long long)nums[i]*k){
+                j++;
+            }
+            ans = min(ans,n-(j-i));
+        }
+
+      return ans;
+        
+    }
+};
